ce2d0cd606f8c17001b4663f60089678f3b4af9d
Olivier <olivier.podeur@gmail.com>
2018-07-14 18:52:21 +0200

    configuration du service de "location". Echec de récupération des permissions avec l'émulateur

--- app/src/main/java/com/example/treasurehuntapp/MapsActivity.java
+++ app/src/main/java/com/example/treasurehuntapp/MapsActivity.java
@@ -1,18 +1,36 @@
 package com.example.treasurehuntapp;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+
+    private FusedLocationProviderClient mFusedLocationClient;
+
+    private static final int PERMISSION_REQUEST_LOCATION_CODE = 1;
+
+
+    private double lastknownLatitude;
+
+    private double lastknownLongitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +40,42 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                String[] permissions = {"composant.permission.CALL_LOCATION"};
+                requestPermissions(permissions, PERMISSION_REQUEST_LOCATION_CODE);
+                Context context = getApplicationContext();
+                Toast toast = Toast.makeText(context, "ACCES LOCATION NOT PERMITTED !", Toast.LENGTH_SHORT);
+                toast.show();
+
+
+            }
+        }
+
+        mFusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+                            // Logic to handle location object
+                            lastknownLatitude = location.getLatitude();
+                            lastknownLongitude = location.getLongitude();
+
+                        }
+                    }
+                });
+
     }
 
 
@@ -39,8 +93,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng lastLocation = new LatLng(lastknownLatitude, lastknownLongitude);
+        mMap.addMarker(new MarkerOptions().position(lastLocation).title("Marker in lastLocation"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(lastLocation));
     }
 }
