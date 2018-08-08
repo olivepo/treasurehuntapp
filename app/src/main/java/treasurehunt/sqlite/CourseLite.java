52384d96f7c43e97278a2a0a61fca2973ad30064
Olivier <olivier.podeur@gmail.com>
2018-08-09 00:32:13 +0200

    modification de l'icone chasse au trésor

---
+++ app/src/main/java/treasurehunt/sqlite/CourseLite.java
@@ -0,0 +1,14 @@
+package treasurehunt.sqlite;
+
+public class CourseLite {
+
+    public String idCourse;
+    public String courseString;
+
+
+    public CourseLite(String idCourse, String courseString) {
+        this.idCourse=idCourse;
+        this.courseString=courseString;
+
+    }
+}
